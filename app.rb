6127054245d9ea2a0700b340dbfbf82ac3c9d38b
antoni668 <antoni668@mail.ru>
2022-03-29 20:43:07 +0600

    Inin Barbers table on configure

--- app.rb
+++ app.rb
@@ -3,6 +3,18 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sqlite3'
 
+def is_barber_exists? db, name
+	db.execute('select * from Barbers where name = ?', [name]).length > 0
+end
+
+def seed_db db, barbers
+	barbers.each do |barber|
+		if !is_barber_exists? db, barber
+			db.execute 'insert into Barbers (name) values (?)', [barber] 
+		end
+	end
+end
+
 def get_db
 	db = SQLite3::Database.new 'base.sqlite'
 	db.results_as_hash = true
@@ -28,11 +40,14 @@ configure do
 		"Message" text
 	)'
 
-	# 	db.execute 'create table if not exists "Barbers"
-	# (
-	# 	"Id" integer primary key autoincrement,
-	# 	"Name" text
-	# ) values (?,?,?)', ['Walter White', 'Jessie Pinkman', 'Gus Fring']
+		db.execute 'create table if not exists "Barbers"
+	(
+		"Id" integer primary key autoincrement,
+		"Name" text
+	)'
+
+	@barber_list = ['Walter White', 'Jessie Pinkman', 'Gus Fring']
+	seed_db db, @barber_list
 
   enable :sessions
 end
